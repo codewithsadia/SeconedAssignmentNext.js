aa0d83a5f462fd420b6a3bdc5e25247eab42a8bc
sadiaimran <sadiaimranmalik24@gmail.com>
2025-01-24 01:35:32 +0500

    update

--- myportfolio/next.config.ts
+++ myportfolio/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "/",
+      },
+    ];
+  },
+  // other config options here
 };
 
 export default nextConfig;
